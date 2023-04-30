3517d3ce334dcd6af3fdbd9ab491a67543d1b706
Maxym Perebiynis <maksperebiynos983@gmail.com>
2023-04-30 17:05:46 +0300

    + migrations

---
+++ Migrations/20230430135233_fixedBoolName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace movie_tracker_website.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixedBoolName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IfMarked",
+                table: "Movies",
+                newName: "IfToWatch");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IfToWatch",
+                table: "Movies",
+                newName: "IfMarked");
+        }
+    }
+}
