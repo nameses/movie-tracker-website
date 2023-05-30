05ef1dc813e280ae05abe9bb260bf6918d11168a
Maxym Perebiynis <maksperebiynos983@gmail.com>
2023-05-30 22:14:45 +0300

    view models fixes (nullable)

--- ViewModels/PagesViews/MoviesViewModel.cs
+++ ViewModels/PagesViews/MoviesViewModel.cs
@@ -2,6 +2,9 @@
 {
     public class MoviesViewModel
     {
-        public AppUserViewModel CurrentUser { get; set; }
+        public AppUserViewModel? CurrentUser { get; set; }
+        public List<MovieViewModel>? Popular { get; set; }
+        public List<MovieViewModel>? WatchedByFriends { get; set; }
+        public List<MovieViewModel>? Recommendations { get; set; }
     }
 }
--- ViewModels/PagesViews/ProfileViewModel.cs
+++ ViewModels/PagesViews/ProfileViewModel.cs
@@ -7,9 +7,9 @@ namespace movie_tracker_website.ViewModels.PagesViews
         public AppUserViewModel? CurrentUser { get; set; }
         public bool? IsUserFollowed { get; set; }
         public AppUserViewModel? UserProfile { get; set; }
-        public List<MovieViewModel?>? FavouriteMovies { get; set; }
-        public List<MovieViewModel?>? RecentMovies { get; set; }
-        public List<AppUserViewModel?>? Followings { get; set; }
+        public List<MovieViewModel>? FavouriteMovies { get; set; }
+        public List<MovieViewModel>? RecentMovies { get; set; }
+        public List<AppUserViewModel>? Followings { get; set; }
         public StatisticViewModel? Statistic { get; set; }
         public List<string?>? Tags { get; set; }
     }
